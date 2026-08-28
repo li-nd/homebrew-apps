2a7c8c4d83c556b84658b40807621ef45ece5abb
Markus Lind <markus@contact.pm>
2026-08-28 16:49:57 +0300

    Update sha256 checksum for Bip39Chiper to match new release

--- Casks/bip39chiper.rb
+++ Casks/bip39chiper.rb
@@ -1,6 +1,6 @@
 cask "bip39chiper" do
   version "1.0.0"
-  sha256 "3862dc2f2381332170fb87273573a5e0681a8cbee66a59ebd4b902e5a03035db"
+  sha256 "d1c9f46d75a11558635abe3fa809c780e970ee149b62df0d911787407b5fd5c6"
 
   url "https://github.com/li-nd/bip39-chiper-mac/releases/download/v#{version}/Bip39Chiper-#{version}-macos.zip"
   name "Bip39Chiper"
